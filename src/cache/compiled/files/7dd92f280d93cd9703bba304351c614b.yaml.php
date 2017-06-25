3a5882673af2f6887f784c99cd87b80e2fef3b89
Thomas Glover <thomas_glover@carmax.com>
2017-06-24 20:54:31 -0400

    Working on php issue

---
+++ src/cache/compiled/files/7dd92f280d93cd9703bba304351c614b.yaml.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/thomasglover/Documents/Projects/www.true-life.church/src/user/config/site.yaml',
+    'modified' => 1493069681,
+    'data' => [
+        'title' => 'Grav',
+        'author' => [
+            'name' => 'Joe Bloggs',
+            'email' => 'joe@test.com'
+        ],
+        'metadata' => [
+            'description' => 'Grav is an easy to use, yet powerful, open source flat-file CMS'
+        ]
+    ]
+];
