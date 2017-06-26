ee454043a85511857fc138e2b550b5d1d22c6861
Thomas Glover <thomas_glover@carmax.com>
2017-06-25 21:10:39 -0400

    working on php faital error

---
+++ src/cache/compiled/files/38246146e74facfc82da6745117f01d7.yaml.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/thomasglover/Documents/Projects/www.true-life.church/src/system/blueprints/config/media.yaml',
+    'modified' => 1493069681,
+    'data' => [
+        'title' => 'PLUGIN_ADMIN.MEDIA',
+        'form' => [
+            'validation' => 'loose',
+            'fields' => NULL
+        ]
+    ]
+];
---
+++ src/cache/compiled/files/dc0839f06dbe361be4040d8da83cde11.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/thomasglover/Documents/Projects/www.true-life.church/src/user/data/licenses.yaml',
+    'modified' => 1498438587,
+    'data' => [
+        
+    ]
+];
