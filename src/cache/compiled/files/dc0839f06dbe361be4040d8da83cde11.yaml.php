1c5939fea65d595b310120c95f97d56897f1efdd
Thomas Glover <thomas_glover@carmax.com>
2017-06-28 14:56:47 -0400

    getting rid of the admin panel

--- src/cache/compiled/files/dc0839f06dbe361be4040d8da83cde11.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/thomasglover/Documents/Projects/www.true-life.church/src/user/data/licenses.yaml',
-    'modified' => 1498438587,
-    'data' => [
-        
-    ]
-];
